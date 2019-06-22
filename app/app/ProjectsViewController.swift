a6ebbbba1a0e464eb41a88dd0923b2fe090c4225
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-06-22 03:11:01 -0400

    Setup UI

---
+++ app/app/ProjectsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProjectsViewController.swift
+//  app
+//
+//  Created by Rushi Gandhi on 2019-06-22.
+//  Copyright © 2019 Rushi Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
