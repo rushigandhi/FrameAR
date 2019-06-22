7624d0084c244f71021c79b03f0af0df7a6ca133
adichha <a22chhab@uwaterloo.ca>
2019-06-22 16:19:09 -0400

    new framebot that works

--- frame-ui/src/App.js
+++ frame-ui/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import graphGit from './Gitgraph'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,18 +20,24 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function App() {
-
   const classes = useStyles();
 
   return (
-    <Grid container
-    direction="column"
-    justify="space-in-between"
-    alignItems="stretch">
-    <Paper className={classes.paper}>xs=12 sm=6</Paper>
-    <Paper className={classes.paper}>xs=12 sm=6</Paper>
-    <Paper className={classes.paper}>xs=12 sm=6</Paper>
-    <Paper className={classes.paper}>xs=12 sm=6</Paper>
-  </Grid>
+    <React.Fragment>
+      <helmet>
+        <style>{'body { background-color: #282c34; }'}</style>
+      </helmet>
+      <CssBaseline />
+      <Container maxWidth="sm">
+      <Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+          <Paper className={classes.paper}>
+            <graphGit/>
+          </Paper>
+        </Grid>
+      </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
--- frame-ui/src/Gitgraph.js
+++ frame-ui/src/Gitgraph.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { Gitgraph } from "@gitgraph/react";
 
 class graphGit extends React.Component{
-    render(){
-        <Gitgraph>{(gitgraph) => {}}</Gitgraph>;
+    return(){
+        <Gitgraph>{(gitgraph) => {
+            
+        }}</Gitgraph>;
     }
 }
 
