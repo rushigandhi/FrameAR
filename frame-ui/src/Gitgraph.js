beb87eb9282e0efe8e984874bc6790ead93a3f3b
adichha <a22chhab@uwaterloo.ca>
2019-06-22 01:16:58 -0400

    ui file + slack bot

---
+++ frame-ui/src/Gitgraph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Gitgraph } from "@gitgraph/react";
+
+class graphGit extends React.Component{
+    render(){
+        <Gitgraph>{(gitgraph) => {}}</Gitgraph>;
+    }
+}
+
+export default graphGit
