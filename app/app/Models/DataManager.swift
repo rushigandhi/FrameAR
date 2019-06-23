c844659336a431d719d07a31a7c7eef392c5f06b
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-06-23 05:05:51 -0400

    Done Deep Linking

--- app/app/Models/DataManager.swift
+++ app/app/Models/DataManager.swift
@@ -8,11 +8,27 @@
 
 import Foundation
 
+class Prelaunch {
+    
+    let projectId: String
+    let commitId: String
+    let file: String
+    
+    init(projectId: String, commitId: String, file: String) {
+        self.projectId = projectId
+        self.commitId = commitId
+        self.file = file
+    }
+    
+}
+
 class DataManager {
     
     // MARK: - Properties
     
     static let shared = DataManager(projects: [])
+    
+    static var prelaunch: Prelaunch? = nil
     
     // MARK: -
     
