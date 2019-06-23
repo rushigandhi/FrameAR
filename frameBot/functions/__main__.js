e55f145480eb0b438db790ab1d36c70b40bf053e
adichha <a22chhab@uwaterloo.ca>
2019-06-22 20:10:12 -0400

    ok we good

--- frameBot/functions/__main__.js
+++ frameBot/functions/__main__.js
@@ -1,27 +1,19 @@
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-const { WebClient } = require('@slack/web-api');
-const web = new WebClient('');
 /**
 * An HTTP endpoint that acts as a webhook for HTTP request event
 * @returns {object} workflow The result of your workflow steps
 */
 module.exports = async (obj) => {
+
   // Prepare workflow object to store API responses
+  
   let workflow = {};
-  
   let jsonObj = JSON.parse(obj);
-  
   // [Workflow Step 1]
   
-  console.log(`Running slack.channels[@0.4.22].messages.create()...`);
+  console.log(`Running slack.channels[@0.4.23].messages.create()...`);
   
-  console.log(web.channels.list);
-  
-  // if(!isChannel(jsonObj.project)){
-  //   newChannel(json.Obj.project)
-  // }
-  
-  workflow.response = await lib.slack.channels['@0.4.22'].messages.create({
+  workflow.response = await lib.slack.channels['@0.4.23'].messages.create({
     channel: `#${jsonObj.project}`,
     text: jsonObj.text,
     attachments: null
@@ -29,15 +21,3 @@ module.exports = async (obj) => {
 
   return workflow;
 };
-
-// async function newChannel(newChan){
-//   workflow.response = await web.channels.create({
-//   name: '${newChan}'
-//  })
-// }
-// 
-// async function isChannel(name){
-//   workflow.response = await web.channels.create({
-//   name: '${newChan}'
-//  })
-// }
